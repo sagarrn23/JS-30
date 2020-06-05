6e9608b295dcbc0bc513e076aea43ec19c2313e0
Sagar Chaudhari <sagarchaudhari250@gmail.com>
2020-06-05 23:26:06 +0530

    Find, findIndex and Splice

--- Day-7/index.js
+++ Day-7/index.js
@@ -14,21 +14,23 @@ const comments = [
 ];
 
 // Some and Every Checks
-// Array.prototype.some() // is at least one person 19 or older?
-// Array.prototype.every() // is everyone 19 or older?
 
+// Array.prototype.some()
 const anyAdult = peoples.some(people => (new Date()).getFullYear() - people.year > 18);
 console.log(anyAdult);
 
+// Array.prototype.every()
 const allAdult = peoples.every(people => (new Date()).getFullYear() - people.year > 18);
 console.log(allAdult);
 
+// Array.prototype.find()
+const find = comments.find(comment => comment.id === 2039842);
+console.log(find);
 
+// Array.prototype.findIndex()
+const elemIndex = comments.findIndex(comment => comment.id === 2039842);
+console.log(elemIndex);
 
-    // Array.prototype.find()
-    // Find is like filter, but instead returns just the one you are looking for
-    // find the comment with the ID of 823423
-
-    // Array.prototype.findIndex()
-    // Find the comment with this ID
-    // delete the comment with the ID of 823423
+// Array.prototype.splice()
+comments.splice(elemIndex, 1)
+console.table(comments);
